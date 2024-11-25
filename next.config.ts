e3ba45d6cb166219f5f35ddcf3148b3dd891a408
Milan Matic <mmatic@aliceandsmithmetalab.com>
2024-11-25 11:15:39 +0100

    next configuration changed

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      "purepng.com",
+      "5.imimg.com",
+      "sportcompactwarehouse.com",
+      "drivesparks.com",
+      "cdn.dicklovett.co.uk",
+      "germanmuscle.co",
+      "www.nationwidevehiclecontracts.co.uk",
+      "cfsm-image-service.s3.amazonaws.com",
+    ],
+  },
 };
 
 export default nextConfig;
