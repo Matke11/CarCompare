9232d13793613e958ee89c6db5acc52bd822caed
Milan Matic <mmatic@aliceandsmithmetalab.com>
2024-11-25 11:46:06 +0100

    cleared code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
 import AllCars from "@/components/AllCars/AllCars";
-// import BannerSection from "@/components/common/BannerSection";
 import { mockCarsData } from "@/data/cars";
 
 export default async function Home() {
   return (
     <div className=" items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-full">
-        {/* <BannerSection
-          bannerTitle="Compare Your Favorite Cars"
-          bannerMessage="Find the best car for your needs with just a few clicks."
-        /> */}
         <AllCars
           title="Compare Your Favorite Cars"
           cars={mockCarsData}
--- src/components/AllCars/AllCars.tsx
+++ src/components/AllCars/AllCars.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useEffect, useState } from "react";
-// import Link from "next/link";
 import SectionHeaderV1 from "../common/SectionHeaderV1";
 import { SelectType } from "../common/common.types";
 import CarCard from "../common/CarCard";
